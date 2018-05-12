7e5c49d91561785d4bf75a0a085237a643dd4945
João Munduruca <joaomunduruca@outlook.com>
2018-05-12 12:33:53 -0300

    Activity de ligação permissao de ligação

---
+++ app/src/main/java/br/com/amigosaude/app/amigosaude/Tel_Activity.java
@@ -0,0 +1,36 @@
+package br.com.amigosaude.app.amigosaude;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Tel_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tel_layout);
+    }
+
+    public void ligar192(View view) {
+        String uri = "tel:" + "192"; // aqui, óbvio, o número para o qual deseja ligar.
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(uri));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
+    }
+}
