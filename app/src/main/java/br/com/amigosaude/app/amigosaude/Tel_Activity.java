9fd57520d896543923c58a115c415f16a5addae7
João Munduruca <joaomunduruca@outlook.com>
2018-06-16 15:45:57 -0300

    Tel_Activity movido // tel_layout alterada

---
+++ app/src/main/java/br/com/amigosaude/app/amigosaude/Tel_Activity.java
@@ -0,0 +1,29 @@
+package br.com.amigosaude.app.amigosaude;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.amigosaude.app.amigosaude.R;
+
+public class Tel_Activity extends AppCompatActivity {
+
+    private static final String TAG = "Tel_Activity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tel_layout);
+    }
+
+    public void ligar(View view) {
+        Log.d(TAG, "ligar: Clicado!");
+    }
+}
