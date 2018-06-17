6135ac7e5755062e89fbee00c85a95517220aa06
YouMustDienow <jee_fdg@hotmail.com>
2018-06-16 21:29:36 -0300

    Adicionada a tela de UBS com algumas unidades. Adicionado/editado o campo de telefones das unidades.

---
+++ app/src/main/java/br/com/amigosaude/app/amigosaude/Ubs_Activiy.java
@@ -0,0 +1,13 @@
+package br.com.amigosaude.app.amigosaude;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ubs_Activiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ubs_layout);
+    }
+}
