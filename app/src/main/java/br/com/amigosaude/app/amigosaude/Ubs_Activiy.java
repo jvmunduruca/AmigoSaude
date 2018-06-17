ecf1acc8eb6256c782823ad215e46528dc0f8619
YouMustDienow <jee_fdg@hotmail.com>
2018-06-17 17:38:41 -0300

    Adicionado metódo de putExtra na classe UBS

--- app/src/main/java/br/com/amigosaude/app/amigosaude/Ubs_Activiy.java
+++ app/src/main/java/br/com/amigosaude/app/amigosaude/Ubs_Activiy.java
@@ -1,13 +1,36 @@
 package br.com.amigosaude.app.amigosaude;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Ubs_Activiy extends AppCompatActivity {
+
+    //variaveis
+    TextView tv;
+    RelativeLayout Unid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ubs_layout);
+    }
+
+    public void informaUnidade1(View view) {
+        tv = findViewById(R.id.jd_aeroporto_end);
+        Unid = findViewById(R.id.unidade1);
+
+        Unid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String unid = tv.getText().toString();
+                Intent i = new Intent(getApplicationContext(), Map_activity.class);
+                i.putExtra("unid", unid);
+                startActivity(i);
+            }
+        });
     }
 }
