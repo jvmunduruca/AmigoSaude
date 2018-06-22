118ddce51df42886e1f11f6ecf48448cd3e2d8ef
João Munduruca <joaomunduruca@outlook.com>
2018-06-22 17:36:59 -0300

    Nomes na Splash // Splash de 3s

--- app/src/main/java/br/com/amigosaude/app/amigosaude/Splash_activity.java
+++ app/src/main/java/br/com/amigosaude/app/amigosaude/Splash_activity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 /**
  * Created by joaom on 22/03/2018.
@@ -20,6 +21,20 @@ public class Splash_activity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
+        String nomes = "Bruno Luiz Conestabile Silva"
+                + System.getProperty ("line.separator")
+                + "Gian Victor de Mello"
+                + System.getProperty ("line.separator")
+                + "Giovana Montes Maletta"
+                + System.getProperty ("line.separator")
+                + "Jeferson Ferreira Lemos"
+                + System.getProperty ("line.separator")
+                + "João Munduruca"
+                + System.getProperty ("line.separator")
+                + "Paula Viana Barros";
+        TextView txt_Nomes = (TextView)findViewById(R.id.nomes);
+        txt_Nomes.setText(nomes);
+
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -32,7 +47,7 @@ public class Splash_activity extends AppCompatActivity{
                 finish();
 
             }
-        }, 2000);
+        }, 3000);
 
     }
 
